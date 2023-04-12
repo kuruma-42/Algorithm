a0c82be5bb9b70cee881c73b6a507838d60be606
kuruma-42 <perfect_yon@naver.com>
2023-04-12 22:26:33 +0900

    study: solve dfs example question

--- Algorithm/dfs.swift
+++ Algorithm/dfs.swift
@@ -49,3 +49,65 @@ func dfs(from: Int) {
 
 dfs(from: 1)
 
+
+/// dfs 예제 문제 풀이 
+var input = readLine()!.split(separator: " ").map{ Int($0)! }
+// Range
+var yRange = input[1]
+var xRange = input[0]
+
+// Set 4 Direction
+var dy = [-1, 0, 1, 0]
+var dx = [0, 1, 0, -1]
+
+var result = 0
+
+// Map
+var map: [[Int]] = Array(repeating: [], count: yRange)
+var visited = Array(repeating: Array(repeating: 0, count: xRange), count: yRange)
+
+for i in 0..<5 {
+    var input = readLine()!.split(separator: " ").map{ Int($0)! }
+    map[i].append(contentsOf: input)
+}
+
+print("\(map)")
+
+func go(y: Int, x: Int) {
+    // 방문처리
+    visited[y][x] = 1
+    
+    for i in 0..<4 {
+        
+        // 4 방향
+        var ny = y + dy[i]
+        var nx = x + dx[i]
+        
+        // Over Flow Check
+        if nx < 0 || nx >= xRange || ny < 0 || ny >= yRange {
+            continue
+        }
+        
+        // Visit Check
+        if visited[ny][nx] == 1 {
+            continue
+        }
+        
+        if map[ny][nx] == 1 && visited[ny][nx] == 0 {
+            go(y: ny, x: nx)
+        }
+    }
+    return
+    
+}
+
+for i in 0..<yRange {
+    for j in 0..<xRange {
+        if map[i][j] == 1 && visited[i][j] == 0 {
+            result += 1
+            go(y: i, x: j)
+        }
+    }
+}
+
+print("result : \(result)")
